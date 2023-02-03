cf8c3fb2441eccf2cbe5c48704e70f0bd9c4b7cb
gustavocotalima <gustavolima@unifei.edu.br>
2023-02-03 14:08:55 -0300

    new transaction modal api connection

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -9,10 +9,17 @@ interface Transaction {
   category: string
   createdAt: string
 }
+interface CreateTransactionInputs {
+  description: string
+  price: number
+  category: string
+  type: 'income' | 'outcome'
+}
 
 interface TransactionsContextType {
   transactions: Transaction[]
   fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (data: CreateTransactionInputs) => Promise<void>
 }
 
 interface TransactionsProviderProps {
@@ -27,9 +34,25 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
   async function fetchTransactions(query?: string) {
-    const response = await api.get('/transactions', { params: { q: query } })
+    const response = await api.get('/transactions', {
+      params: { _sort: 'createdAt', _order: 'desc', q: query },
+    })
 
     setTransactions(response.data)
+  }
+
+  async function createTransaction(data: CreateTransactionInputs) {
+    const { description, price, category, type } = data
+
+    const response = await api.post('/transactions', {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date(),
+    })
+
+    setTransactions((state) => [response.data, ...state])
   }
 
   useEffect(() => {
@@ -37,7 +60,9 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransactions, createTransaction }}
+    >
       {children}
     </TransactionsContext.Provider>
   )
